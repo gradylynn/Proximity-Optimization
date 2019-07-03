9588d14e65f01e4a2cfc8f27a8fbecc787e70a7e
Grady Lynn <gradylynn314@gmail.com>
2019-07-03 15:58:18 -0400

    Changed stuff to make it better.

--- computeProximity.py
+++ computeProximity.py
@@ -8,12 +8,18 @@ Proximity Optimization function.
 from collections import deque
 import numpy as np
 from scipy.spatial import distance
-
+from heapq import heappush, heappop
+import math
 
 def proximity(xy_dims, featureList, scale=20):
     assert len(xy_dims)==2 and xy_dims[0]>=0 and xy_dims[1]>=0 , "Incorrectly formatted chip dimmensions. Dims must be non-negative x and y dimensions."
     
+    def isInChip(point):
+        return point[0] >= 0 and point[0] < xy_dims[0] and point[1] >= 0 and point[1] < xy_dims[1]
+    
     ans = np.full(xy_dims, np.inf)
+    global countChanged, c
+#    visited = np.full(xy_dims, 0)
     
     def getQuadrant(origin, point):
         if point == origin: return 0
@@ -25,9 +31,6 @@ def proximity(xy_dims, featureList, scale=20):
         if point[0] < origin[0] and point[1] < origin[1] and point[0] - origin[0] >= point[1] - origin[1]: return 6
         if point[0] >= origin[0] and point[1] < origin[1] and -(point[0] - origin[0]) > point[1] - origin[1]: return 7
         if point[0] > origin[0] and point[1] < origin[1] and -(point[0] - origin[0]) <= point[1] - origin[1]: return 8 
-    
-    def isInChip(point):
-        return point[0] >= 0 and point[0] < xy_dims[0] and point[1] >= 0 and point[1] < xy_dims[1]
         
     up = []
     down = []
@@ -42,38 +45,48 @@ def proximity(xy_dims, featureList, scale=20):
     
     def getNearestSides(outsidePoint):
         if outsidePoint[0] < 0:
-            if outsidePoint[1] < 0: return left + down
-            elif outsidePoint[1] >= xy_dims[1]: return left[::-1] + up
+            if outsidePoint[1] < 0: return left + down[1:]
+            elif outsidePoint[1] >= xy_dims[1]: return left + up[1:]
             else: return left
         elif outsidePoint[0] >= xy_dims[0]:
-            if outsidePoint[1] < 0: return right + down[::-1]
-            elif outsidePoint[1] >= xy_dims[1]: return right[::-1] + up[::-1]
+            if outsidePoint[1] < 0: return right[1:] + down
+            elif outsidePoint[1] >= xy_dims[1]: return right[:-1] + up
             else: return right
         else:
             if outsidePoint[1] < 0: return down
             else: return up
     
-    def solvePoint(feature, point):
+    def solvePoint(x):
+        global countChanged, c
+        
+        feature = x[1][0]
+        point = x[1][1]
+        d = x[0]
+        
+        c += 1
+        
         if not isInChip(point): return
         
-        d = distance.euclidean(feature, point) * scale
+#        visited[point[0], point[1]] += 1
+        
         if d < ans[point[0], point[1]]:
             ans[point[0], point[1]] = d
+            countChanged += 1
         else: return
         
         quad = getQuadrant(feature, point)
         
         if quad == 0:
-            #Diagonals
-            q.append((feature, (point[0] + 1, point[1] + 1))) # Quadrant 2
-            q.append((feature, (point[0] - 1, point[1] + 1))) # Quadrant 4
-            q.append((feature, (point[0] - 1, point[1] - 1))) # Quadrant 6
-            q.append((feature, (point[0] + 1, point[1] - 1))) # Quadrant 8
             #Adjacents
-            q.append((feature, (point[0] + 1, point[1]))) # Quadrant 1
-            q.append((feature, (point[0], point[1] + 1))) # Quadrant 3
-            q.append((feature, (point[0] - 1, point[1]))) # Quadrant 5
-            q.append((feature, (point[0], point[1] - 1))) # Quadrant 7
+            heappush(q, (scale, (feature, (point[0] + 1, point[1]))) ) # Quadrant 1
+            heappush(q, (scale, (feature, (point[0], point[1] + 1)))) # Quadrant 3
+            heappush(q, (scale, (feature, (point[0] - 1, point[1])))) # Quadrant 5
+            heappush(q, (scale, (feature, (point[0], point[1] - 1)))) # Quadrant 7
+            #Diagonals
+            heappush(q, (math.sqrt(2)*scale, (feature, (point[0] + 1, point[1] + 1)))) # Quadrant 2
+            heappush(q, (math.sqrt(2)*scale, (feature, (point[0] - 1, point[1] + 1)))) # Quadrant 4
+            heappush(q, (math.sqrt(2)*scale, (feature, (point[0] - 1, point[1] - 1)))) # Quadrant 6
+            heappush(q, (math.sqrt(2)*scale, (feature, (point[0] + 1, point[1] - 1)))) # Quadrant 8
             return
         elif quad == 1:
             pushFirst = (feature, (point[0] + 1, point[1]))
@@ -100,93 +113,93 @@ def proximity(xy_dims, featureList, scale=20):
             pushFirst = (feature, (point[0] + 1, point[1]))
             pushSecond = (feature, (point[0] + 1, point[1] - 1))
 
-        if q[-1] == pushFirst and len(q) > 0:
-            q.append(pushSecond)
-            return
-        else:
-            q.append(pushFirst)
-            q.append(pushSecond)
-            return
-  
-    q = deque()
+        heappush(q, (distance.euclidean(pushFirst[0], pushFirst[1]) * scale, pushFirst))
+        heappush(q, (distance.euclidean(pushSecond[0], pushSecond[1]) * scale, pushSecond))
+        return
+        
+    q = []
+    countChanged = 0
+    c = 0
     for feature in featureList:
         if isInChip(feature):
-            q.append((feature, feature))
+            heappush(q, (0, (feature, feature)) )
         else:
             for edgePoint in getNearestSides(feature):
-                q.append((feature, edgePoint))
+                heappush(q, (distance.euclidean(feature, edgePoint)*scale, (feature, edgePoint)))
         
-    while len(q) > 0:
-        x = q.popleft()
-        solvePoint(x[0], x[1])
+    while countChanged < xy_dims[0] * xy_dims[1]:
+        x = heappop(q)
+        solvePoint(x)
 
+#    print(visited)
+    print(c)
     return ans
 
 
 # This don't work as well as the above method...
-def computeProximity(xy_dims, featureList, scale=20):
-    assert len(xy_dims)==2 and xy_dims[0]>=0 and xy_dims[1]>=0 , "Incorrectly formatted chip dimmensions. Dims must be non-negative x and y dimensions."
-    
-    ans = np.full(xy_dims, np.inf)
-    
-    def isInChip(point):
-        return point[0] >= 0 and point[0] < xy_dims[0] and point[1] >= 0 and point[1] < xy_dims[1]
-    
-    def nearestPointInChip(feature):
-        if isInChip(feature): return feature
-        x = min(max(0, feature[0]), xy_dims[0])
-        y = min(max(0, feature[1]), xy_dims[1])
-        return (x, y)
-    
-    def solveQuadrant(feature, deque, quadrant):
-        point = deque.popleft()
-        if not isInChip(point): return
-        
-        d = distance.euclidean(feature, point) * scale
-        if d < ans[point[0], point[1]]:
-            ans[point[0], point[1]] = d
-        else: return
-        
-        if quadrant == 1:
-            pushFirst = (point[0] + 1, point[1]) # 
-            pushSecond = (point[0], point[1] + 1) # 
-        elif quadrant == 2:
-            pushFirst = (point[0], point[1] + 1)
-            pushSecond = (point[0] - 1, point[1])
-        elif quadrant == 3:
-            pushFirst = (point[0] - 1, point[1])
-            pushSecond = (point[0], point[1] - 1)
-        elif quadrant == 4:
-            pushFirst = (point[0], point[1] - 1)
-            pushSecond = (point[0] + 1, point[1])
-            
-        if len(deque) > 0 and deque[-1] == pushFirst:
-            deque.append(pushSecond)
-            return
-        else:
-            deque.append(pushFirst)
-            deque.append(pushSecond)
-            return
-        
-    q = deque()
-    for feature in featureList:
-        startOnChip = nearestPointInChip(feature)
-        ans[startOnChip[0], startOnChip[1]] = distance.euclidean(feature, startOnChip)
-        
-        q.append((startOnChip[0] + 1, startOnChip[1]))
-        while len(q) > 0:
-            solveQuadrant(feature, q, 1)
-        
-        q.append((startOnChip[0], startOnChip[1] + 1))
-        while len(q) > 0:
-            solveQuadrant(feature, q, 2)
-           
-        q.append((startOnChip[0] - 1, startOnChip[1]))    
-        while len(q) > 0:
-            solveQuadrant(feature, q, 3)
-            
-        q.append((startOnChip[0], startOnChip[1] - 1))    
-        while len(q) > 0:
-            solveQuadrant(feature, q, 4)
-            
-    return ans
+#def computeProximity(xy_dims, featureList, scale=20):
+#    assert len(xy_dims)==2 and xy_dims[0]>=0 and xy_dims[1]>=0 , "Incorrectly formatted chip dimmensions. Dims must be non-negative x and y dimensions."
+#    
+#    ans = np.full(xy_dims, np.inf)
+#    
+#    def isInChip(point):
+#        return point[0] >= 0 and point[0] < xy_dims[0] and point[1] >= 0 and point[1] < xy_dims[1]
+#    
+#    def nearestPointInChip(feature):
+#        if isInChip(feature): return feature
+#        x = min(max(0, feature[0]), xy_dims[0])
+#        y = min(max(0, feature[1]), xy_dims[1])
+#        return (x, y)
+#    
+#    def solveQuadrant(feature, deque, quadrant):
+#        point = deque.popleft()
+#        if not isInChip(point): return
+#        
+#        d = distance.euclidean(feature, point) * scale
+#        if d < ans[point[0], point[1]]:
+#            ans[point[0], point[1]] = d
+#        else: return
+#        
+#        if quadrant == 1:
+#            pushFirst = (point[0] + 1, point[1]) # 
+#            pushSecond = (point[0], point[1] + 1) # 
+#        elif quadrant == 2:
+#            pushFirst = (point[0], point[1] + 1)
+#            pushSecond = (point[0] - 1, point[1])
+#        elif quadrant == 3:
+#            pushFirst = (point[0] - 1, point[1])
+#            pushSecond = (point[0], point[1] - 1)
+#        elif quadrant == 4:
+#            pushFirst = (point[0], point[1] - 1)
+#            pushSecond = (point[0] + 1, point[1])
+#            
+#        if len(deque) > 0 and deque[-1] == pushFirst:
+#            deque.append(pushSecond)
+#            return
+#        else:
+#            deque.append(pushFirst)
+#            deque.append(pushSecond)
+#            return
+#        
+#    q = deque()
+#    for feature in featureList:
+#        startOnChip = nearestPointInChip(feature)
+#        ans[startOnChip[0], startOnChip[1]] = distance.euclidean(feature, startOnChip)
+#        
+#        q.append((startOnChip[0] + 1, startOnChip[1]))
+#        while len(q) > 0:
+#            solveQuadrant(feature, q, 1)
+#        
+#        q.append((startOnChip[0], startOnChip[1] + 1))
+#        while len(q) > 0:
+#            solveQuadrant(feature, q, 2)
+#           
+#        q.append((startOnChip[0] - 1, startOnChip[1]))    
+#        while len(q) > 0:
+#            solveQuadrant(feature, q, 3)
+#            
+#        q.append((startOnChip[0], startOnChip[1] - 1))    
+#        while len(q) > 0:
+#            solveQuadrant(feature, q, 4)
+#            
+#    return ans
